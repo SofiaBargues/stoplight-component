ce21789d3aa42da996fc65bd02c94f24c3e61a29
Sofia Bargues <barguessofia@gmail.com>
2024-09-05 00:11:36 +0100

    before the caos

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
 import { useEffect, useState } from "react";
 
-function stopLigth(initialColor: string) {
-  <div className=" m-auto flex flex-col gap-3">
-    <div
-      style={
-        initialColor === "#ea3324"
-          ? { background: "#ea3324" }
-          : { background: "#7f7f7f" }
-      }
-      className="rounded-full h-14 w-14"
-    ></div>
-    <div
-      style={
-        initialColor === "#fffe57"
-          ? { background: "#fffe57" }
-          : { background: "#7f7f7f" }
-      }
-      className="rounded-full h-14 w-14"
-    ></div>
-    <div
-      style={
-        initialColor === "#387e23"
-          ? { background: "#387e23" }
-          : { background: "#7f7f7f" }
-      }
-      className="rounded-full h-14 w-14"
-    ></div>
-  </div>;
+function StopLight({ initialColor }: { initialColor: string }) {
+  return (
+    <div className=" m-auto flex flex-col gap-3">
+      <div
+        style={
+          initialColor === "#ea3324"
+            ? { background: "#ea3324" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+      <div
+        style={
+          initialColor === "#fffe57"
+            ? { background: "#fffe57" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+      <div
+        style={
+          initialColor === "#387e23"
+            ? { background: "#387e23" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+    </div>
+  );
 }
 
 function App() {
-  const [light, setLight] = useState("#ea3324");
+  const [light, setLight] = useState(["#ea3324", "#fffe57"]);
 
   useEffect(() => {
-    // Use setTimeout to update the message after 2000 milliseconds (2 seconds)
     const changingLight =
       light === "#ea3324"
         ? "#387e23"
@@ -50,89 +51,21 @@ function App() {
       setLight(changingLight);
     }, duration);
     console.log(duration, light);
-    // Cleanup function to clear the timeout if the component unmounts
     return () => clearTimeout(timeoutId);
-  }, [light]); // Empty dependency array ensures the effect runs only once
+  }, [light]);
 
   return (
     <div className="justify-center h-screen flex  ">
-      <div className=" m-auto flex flex-col gap-3">
-        <div
-          style={
-            light === "#ea3324"
-              ? { background: "#ea3324" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#fffe57"
-              ? { background: "#fffe57" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#387e23"
-              ? { background: "#387e23" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-      </div>
-      <div className=" m-auto flex flex-col gap-3">
-        <div
-          style={
-            light === "#ea3324"
-              ? { background: "#ea3324" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#fffe57"
-              ? { background: "#fffe57" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#387e23"
-              ? { background: "#387e23" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-      </div>
-      <div className=" m-auto flex flex-col gap-3">
-        <div
-          style={
-            light === "#ea3324"
-              ? { background: "#ea3324" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#fffe57"
-              ? { background: "#fffe57" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
-        <div
-          style={
-            light === "#387e23"
-              ? { background: "#387e23" }
-              : { background: "#7f7f7f" }
-          }
-          className="rounded-full h-14 w-14"
-        ></div>
+      <div
+        className=" m-auto flex  gap-8
+      "
+      >
+        <StopLight initialColor={light} />
+        <StopLight initialColor={light} />
+        <StopLight initialColor={light} />
+        <StopLight initialColor={light} />
+        <StopLight initialColor={light} />
+        <StopLight initialColor={light} />
       </div>
     </div>
   );
