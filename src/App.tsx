ac953a48c0aab1e2fd881b5100d591b6fc91336f
Sofia Bargues <barguessofia@gmail.com>
2024-09-06 10:27:59 +0100

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
 import { useEffect, useState } from "react";
-
-function StopLight({ initialColor }: { initialColor: string }) {
-  return (
-    <div className=" m-auto flex flex-col gap-3">
-      <div
-        style={
-          initialColor === "#ea3324"
-            ? { background: "#ea3324" }
-            : { background: "#7f7f7f" }
-        }
-        className="rounded-full h-14 w-14"
-      ></div>
-      <div
-        style={
-          initialColor === "#fffe57"
-            ? { background: "#fffe57" }
-            : { background: "#7f7f7f" }
-        }
-        className="rounded-full h-14 w-14"
-      ></div>
-      <div
-        style={
-          initialColor === "#387e23"
-            ? { background: "#387e23" }
-            : { background: "#7f7f7f" }
-        }
-        className="rounded-full h-14 w-14"
-      ></div>
-    </div>
-  );
-}
-
 const lights = ["#ea3324", "#fffe57", "#387e23"];
 
 function randomLight() {
   return lights[Math.floor(Math.random() * lights.length)];
 }
 
-function App() {
-  const [light, setLight] = useState(randomLight());
+function StopLight({ initialColor }: { initialColor: string }) {
+  const [light, setLight] = useState(initialColor);
   // const [light, setLight] = useState("#ea3324");
 
   useEffect(() => {
@@ -60,7 +28,37 @@ function App() {
     console.log(duration, light);
     return () => clearTimeout(timeoutId);
   }, [light]);
+  return (
+    <div className=" m-auto flex flex-col gap-3">
+      <div
+        style={
+          light === "#ea3324"
+            ? { background: "#ea3324" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+      <div
+        style={
+          light === "#fffe57"
+            ? { background: "#fffe57" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+      <div
+        style={
+          light === "#387e23"
+            ? { background: "#387e23" }
+            : { background: "#7f7f7f" }
+        }
+        className="rounded-full h-14 w-14"
+      ></div>
+    </div>
+  );
+}
 
+function App() {
   return (
     <div className="justify-center h-screen flex  ">
       <div
@@ -71,8 +69,7 @@ function App() {
         <StopLight initialColor={randomLight()} />
         <StopLight initialColor={randomLight()} />
         <StopLight initialColor={randomLight()} />
-        <StopLight initialColor={light} />
-        <StopLight initialColor={light} />
+        <StopLight initialColor={randomLight()} />
       </div>
     </div>
   );
